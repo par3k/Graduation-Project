9a17f3a9c094b3bdb33f9606ab04e4cb600d24ca
Hoijai Park (Alex) <52132138+par3k@users.noreply.github.com>
2020-08-21 22:47:15 +0900

    Update config.py

--- Flask Web Server/App/config.py
+++ Flask Web Server/App/config.py
@@ -1,6 +1,6 @@
 class Config(object):
     SECRET_KEY = '608f10b6e0f01d77436fb7226bc46b86'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://alex:ghlwo7831@13.125.21.190/graduation'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql:/your sql addr'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CSRF_ENABLED = True
